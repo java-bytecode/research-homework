0c7f7c38268c32e250d27fa187ea4cba8deddbbc
java-bytecode <ntwrkleapswitch@gmail.com>
2020-09-17 02:25:00 -0500

    second commit

--- src/javaOops/CarDriverCode.java
+++ src/javaOops/CarDriverCode.java
@@ -10,7 +10,13 @@ package javaOops;
 public class CarDriverCode {
 	
 	public static void main(String[] args) {
+		CarPanel obj = new CarPanel();
 		
+		obj.carStatus();
+		System.out.println(obj.carID);
+		System.out.println(obj.noOfSeats);
+		System.out.println(obj.vehicleName);
+		System.out.println(obj.wheelDrive);
 	}
 	
 
