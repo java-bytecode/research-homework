e44f729e730f1cfdeac507d1bbbc31c893768b8d
java-bytecode <ntwrkleapswitch@gmail.com>
2020-09-23 20:08:06 -0500

    add stacktrace()

--- src/com/cyberxnuke/threadPrograms/DriverCode.java
+++ src/com/cyberxnuke/threadPrograms/DriverCode.java
@@ -1,25 +1,32 @@
 package com.cyberxnuke.threadPrograms;
 
+import java.util.Set;
+
 public class DriverCode {
 
     public static void main(String[] args) throws InterruptedException {
+
         // Thread 1
         threadP1 obj = new threadP1();
         obj.setName("Hello");
         obj.start();
-        //obj.join(); // Why?
+        obj.join(); // Why?
 
         // Thread 3
         threadP3 obj3 = new threadP3();
-        obj3.start();
-       // obj3.join();
-
-        // Thread 2
         threadP2 obj2 = new threadP2();
         Thread thread2 = new Thread(obj2);
+        obj3.start();
+        // obj3.join();
+
+        // Thread 2
+
         thread2.setPriority(Thread.MAX_PRIORITY);
         thread2.start();
-       // thread2.join();
+//        System.out.println(thread2.getName());
+//        System.out.println(obj.getName());
+//        System.out.println(obj3.getName());
+        // thread2.join();
 
         System.out.println("HELLOWOW");
 
@@ -28,6 +35,19 @@ public class DriverCode {
         System.out.println("Thread 1 is alive? " + obj.isAlive());
         System.out.println("Thread 3 is alive? " + obj3.isAlive());
         System.out.println("Name of Thread 1: " + obj.getName());
+        System.out.println("Thread 1 ID: " + obj.getId());
+        System.out.println("Thread 2 ID: " + thread2.getId());
+        System.out.println("Thread 3 ID: " + obj3.getId());
+
+        System.out.println("\n" + "ID" + "\t\t\t\t" + " Name");
+
+        Set<Thread> threadPropertySet = threadP1.getAllStackTraces().keySet();
+
+        for (Thread thr : threadPropertySet) {
+            System.out.println(thr.getId() + "\t\t\t\t" + thr.getName() + "\t\t\t\t" + thr.getState());
+        }
+
+        System.out.println();
 
         // Priority
         System.out.println("Priority Of Thread 1: " + obj.getPriority());
@@ -41,4 +61,5 @@ public class DriverCode {
         System.out.println("Thread 1 is daemon thread? " + obj.isDaemon());
         System.out.println("Thread 4 is daemon thread? " + obj4.isDaemon());
     }
+
 }
